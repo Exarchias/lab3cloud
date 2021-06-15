96f5eb404214b6195fcfdfe27960cf0a91f1eda6
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2021-06-15 21:12:27 +0200

    lab 3 is completed
    I may come with a new version just to make it better, (a more dynamic version)

--- server.js
+++ server.js
@@ -7,26 +7,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const SERVER_PORT = process.env.PORT || 3000;
-const MAX_LENGTH_WORD = 10;
+var MAX_LENGTH_WORD = 11;
 
 app.post('/getWordLengthFrequency', function (req, res) {
 	console.log("post requested received with data: ");
     
     var data = req.body.data;
+    //MAX_LENGTH_WORD = data.length;
     
     console.log(data);
-    
+    //result.fill(0);   
+    var result2 = data.split(" ");
     var result = new Array(MAX_LENGTH_WORD);
-    result.fill(0);   
+    result.fill(0); 
     
-    var resultStr = "";
+    //var resultStr = "";
+
+    for (var i = 0; i < result2.length; i++) {
+        //resultStr = resultStr + result[i] + " ";
+        var word = result2[i];
+        var index = word.length;
+        console.log(word + ":" + index);
+        result[index] = result[index] + 1;
+    }
+
+    var resultStr2 = "";
 
     for (var i = 0; i < MAX_LENGTH_WORD; i++) {
-        resultStr = resultStr + result[i] + " ";
+        resultStr2 = resultStr2 + result[i] + " ";
     }
   
     console.log("sending response");
-    res.send(resultStr);
+    res.send(resultStr2);
     res.end();
  
 })
